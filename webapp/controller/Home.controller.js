46021ad72953e22e8e335511a0511ca2690c0a25
Juan Rabelo <juanrodrigues11314@hotmail.com>
2025-02-23 19:57:43 +0000

    feat - add projects and experiences section

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,30 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/GroupHeaderListItem",
+    "sap/uxap/BlockBase",
+    "sap/m/Text",
+    "sap/ui/core/CustomData",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, GroupHeaderListItem, BlockBase, Text, CustomData) {
+    "use strict";
 
-        return Controller.extend("com.rrsolutions.myportfolio.controller.Home", {
-            onInit: function () {
+    return Controller.extend("com.rrsolutions.myportfolio.controller.Home", {
+      onInit: function () {},
 
-            }
+      /**
+       * @override
+       */
+      onAfterRendering() {},
+
+      getGroupHeader: function (oGroup) {
+        return new GroupHeaderListItem({
+          title: oGroup.key,
         });
+      },
     });
+  }
+);
