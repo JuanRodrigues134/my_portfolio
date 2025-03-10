96fe7235088cb2b60a28acecb50ae696d64a512f
JuanRabelo134 <juanrodrigues11314@hotmail.com>
2025-03-10 11:38:59 +0000

    refactor - create BaseController

---
+++ controller/BaseController.js
@@ -0,0 +1,15 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+
+  return Controller.extend(
+    "com.rrsolutions.myportfolio.controller.BaseController",
+    {
+      getModel: function (sName) {
+        return this.getView().getModel(sName);
+      },
+      setModel: function (oModel, sName) {
+        this.getView().setModel(oModel, sName);
+      },
+    }
+  );
+});
--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
   [
-    "sap/ui/core/mvc/Controller",
+    "com/rrsolutions/myportfolio/controller/BaseController",
     "sap/m/GroupHeaderListItem",
     "sap/uxap/BlockBase",
     "sap/m/Text",
@@ -14,7 +14,7 @@ sap.ui.define(
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
   function (
-    Controller,
+    BaseController,
     GroupHeaderListItem,
     BlockBase,
     Text,
@@ -28,79 +28,84 @@ sap.ui.define(
 
     const URLHelper = library.URLHelper;
 
-    return Controller.extend("com.rrsolutions.myportfolio.controller.Home", {
-      onInit: function () {
-        const oViewModel = new JSONModel({
-          isDarkMode: false,
-        });
-        this.getView().setModel(oViewModel, "viewModel");
-      },
-      /**
-       * @override
-       */
-      onAfterRendering() {},
-      onCvLinkPress: async function () {
-        const oView = this.getView();
-        const oLink = this.byId("idCvLink");
-        this._oFragment ??= await Fragment.load({
-          id: oView.getId(),
-          name: "com.rrsolutions.myportfolio.view.fragments.SelectLanguagePopover",
-          controller: this,
-        });
-        oView.addContent(this._oFragment);
-        this._oFragment.openBy(oLink);
-      },
-      onDownloadCv: function (sLanguage) {
-        const sFileName = `JUAN RABELO ${sLanguage}.pdf`;
-        const sFilePath = jQuery.sap.getModulePath(
-          "com.rrsolutions.myportfolio",
-          "/assets/" + sFileName
-        );
-        const oLink = document.createElement("a");
+    return BaseController.extend(
+      "com.rrsolutions.myportfolio.controller.Home",
+      {
+        onInit: function () {
+          const oViewModel = new JSONModel({
+            isDarkMode: false,
+          });
+          this.setModel(oViewModel, "viewModel");
+        },
+        /**
+         * @override
+         */
+        onAfterRendering() {},
+        onCvLinkPress: async function () {
+          const oView = this.getView();
+          const oLink = this.byId("idCvLink");
+          this._oFragment ??= await Fragment.load({
+            id: oView.getId(),
+            name: "com.rrsolutions.myportfolio.view.fragments.SelectLanguagePopover",
+            controller: this,
+          });
+          oView.addContent(this._oFragment);
+          this._oFragment.openBy(oLink);
+        },
+        onDownloadCv: function (sLanguage) {
+          const sFileName = `JUAN RABELO ${sLanguage}.pdf`;
+          const sFilePath = jQuery.sap.getModulePath(
+            "com.rrsolutions.myportfolio",
+            "/assets/" + sFileName
+          );
+          const oLink = document.createElement("a");
 
-        oLink.href = sFilePath;
-        oLink.download = sFileName;
-        document.body.appendChild(oLink);
-        oLink.click();
-        document.body.removeChild(oLink);
-      },
-      onMyAppGenericTilePress: function (oEvent) {
-        const oSource = oEvent.getSource();
-        const { id, name, url } = oSource.getBindingContext("user").getObject();
-        URLHelper.redirect(url, true);
-      },
-      onPressChangeLanguage: async function () {
-        const oView = this.getView();
-        this._oLanguageList ??= await Fragment.load({
-          id: oView.getId(),
-          name: "com.rrsolutions.myportfolio.view.fragments.SelectLanguageList",
-          controller: this,
-        });
-        oView.addContent(this._oLanguageList);
-        this._oLanguageList.open();
-      },
-      onPressSelectLanguage: function () {
-        const sSelectedLanguage = this.byId("idSlctLanguage");
-        sap.ui.getCore().setLanguage(sSelectedLanguage);
-        models.loadUserModel();
-        this.getView().getModel("user").refresh();
-      },
-      onPressChangeTheme: function (sTheme) {
-        const oViewModel = this.getView().getModel("viewModel");
-        const isDarkMode = oViewModel.getProperty("/isDarkMode");
-        oViewModel.setProperty("/isDarkMode", !isDarkMode);
-        sap.ui.getCore().getConfiguration().setTheme(sTheme);
-      },
-      onPressOpenLinks: async function () {
-        const oView = this.getView();
-        this._oActionFragment ??= await Fragment.load({
-          id: oView.getId(),
-          name: "com.rrsolutions.myportfolio.view.fragments.Header.MobileLinks",
-          controller: this,
-        });
-        oView.addContent(this._oActionFragment);
-        this._oActionFragment.openBy(this.byId("idButtonLinks"));
-      },
-    });
+          oLink.href = sFilePath;
+          oLink.download = sFileName;
+          document.body.appendChild(oLink);
+          oLink.click();
+          document.body.removeChild(oLink);
+        },
+        onMyAppGenericTilePress: function (oEvent) {
+          const oSource = oEvent.getSource();
+          const { id, name, url } = oSource
+            .getBindingContext("user")
+            .getObject();
+          URLHelper.redirect(url, true);
+        },
+        onPressChangeLanguage: async function () {
+          const oView = this.getView();
+          this._oLanguageList ??= await Fragment.load({
+            id: oView.getId(),
+            name: "com.rrsolutions.myportfolio.view.fragments.SelectLanguageList",
+            controller: this,
+          });
+          oView.addContent(this._oLanguageList);
+          this._oLanguageList.open();
+        },
+        onPressSelectLanguage: function () {
+          const sSelectedLanguage = this.byId("idSlctLanguage");
+          sap.ui.getCore().setLanguage(sSelectedLanguage);
+          models.loadUserModel();
+          this.getModel("user").refresh();
+        },
+        onPressChangeTheme: function (sTheme) {
+          const oViewModel = this.getModel("viewModel");
+          const isDarkMode = oViewModel.getProperty("/isDarkMode");
+          oViewModel.setProperty("/isDarkMode", !isDarkMode);
+          sap.ui.getCore().getConfiguration().setTheme(sTheme);
+        },
+        onPressOpenLinks: async function () {
+          const oView = this.getView();
+          this._oActionFragment ??= await Fragment.load({
+            id: oView.getId(),
+            name: "com.rrsolutions.myportfolio.view.fragments.Header.MobileLinks",
+            controller: this,
+          });
+          oView.addContent(this._oActionFragment);
+          this._oActionFragment.openBy(this.byId("idButtonLinks"));
+        },
+      }
+    );
   }
 );
